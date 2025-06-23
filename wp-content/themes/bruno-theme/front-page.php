67c63d29d8331282176c0037e83f0bb67194edf2
Bruno <bruno.etcheverry@hotmail.fr>
2025-06-23 19:31:20 +0200

    Page d'accueil #1

--- wp-content/themes/bruno-theme/front-page.php
+++ wp-content/themes/bruno-theme/front-page.php
@@ -1,12 +1,19 @@
 <?php get_header(); ?>
 
-<div class="homepage-content">
+<div class="homepage-content content-wrapper bordered">
+
+    <section class="hero">
+        <div class="hero-inner">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/face.jpg" alt="Bruno Etcheverry" class="hero-photo">
+            <h1>Bruno Etcheverry</h1>
+            <p>Conseiller immobilier indépendant affilié IAD France</p>
+        </div>
+    </section>
+
   <?php
-
-  if(!empty(get_the_content())){
-        the_content();
-  }
-
+    while (have_posts()) : the_post();
+      the_content(); // Elementor injectera ici ton contenu personnalisé
+    endwhile;
   ?>
 </div>
 
--- wp-content/themes/bruno-theme/index.php
+++ wp-content/themes/bruno-theme/index.php
@@ -1,5 +1,13 @@
 <?php get_header(); ?>
 
-<p>Contenu principal</p>
+<main class="site-main">
+  <div class="container content-wrapper bordered">
+   <?php 
+        if(!empty(get_the_content())){
+                the_content();
+        }
+    ?>
+  </div>
+</main>
 
 <?php get_footer(); ?>
